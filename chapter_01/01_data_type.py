def25b8acbe75c721dec3d7097e3f5f01ad13389
niraj <nirajnsp5@gmail.com>
2024-12-04 00:09:26 +0530

    initial commmit

---
+++ chapter_01/01_data_type.py
@@ -0,0 +1,44 @@
+# Data Type In python 
+
+# Number
+
+## 1. integer(int) 
+
+a = 2
+print(type(a)) # <class 'int'>
+
+
+a = 7765924532662678677504600438
+
+print(type(a)) # <class 'int'>
+
+## float (float)
+
+
+b = 0.234
+
+print(type(b)) # output <class 'float'>
+
+
+## complex represnt complex number of maths
+
+c = complex(2,1)
+
+print(c,type(c))
+
+
+# Strings
+
+s = 'hello '
+
+print(type(s))
+
+
+# Boolean
+
+b = False
+c = True
+
+print(type(b))
+
+print(type(c))
